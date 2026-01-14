5c439220a6254f5ad733518b5cea80d2c431afc1
joshuawprichard <joshuawprichard@gmail.com>
2026-01-14 17:15:36 +1300

    [Fix] Remote remote exec block as it was causing issues.

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -88,23 +88,23 @@ resource "aws_instance" "app" {
               # You'll need to set this up via SSH after initial deployment
               EOF
 
- # ---- REMOTE EXEC BLOCK ----
-  provisioner "remote-exec" {
-    connection {
-      type        = "ssh"
-      host        = self.public_ip          # Must be reachable
-      user        = "ec2-user"
-      private_key = file("~/.ssh/id_rsa")   # Path to your key
-    }
+#  # ---- REMOTE EXEC BLOCK ----
+#   provisioner "remote-exec" {
+#     connection {
+#       type        = "ssh"
+#       host        = self.public_ip          # Must be reachable
+#       user        = "ec2-user"
+#       private_key = file("~/.ssh/id_rsa")   # Path to your key
+#     }
 
-    # Commands to run after instance is up
-    inline = [
-      "docker --version",
-      "docker-compose --version",
-      "cd /home/ec2-user/app && docker-compose up -d",
-      "cd /home/ec2-user/app/backend && npx prisma migrate deploy"
-    ]
-  }
+#     # Commands to run after instance is up
+#     inline = [
+#       "docker --version",
+#       "docker-compose --version",
+#       "cd /home/ec2-user/app && docker-compose up -d",
+#       "cd /home/ec2-user/app/backend && npx prisma migrate deploy"
+#     ]
+#   }
 
   tags = {
     Name = "budget-tracker-app"
