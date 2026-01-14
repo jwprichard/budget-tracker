5e9d4fc887149a9b7a47fbf3722169f4909590e6
joshuawprichard <joshuawprichard@gmail.com>
2026-01-15 07:22:18 +1300

    [Fix] Allow RDS access from EC2 and public internet for debugging
    Changes:
    - Added EC2 security group to RDS ingress (fixes backend connection)
    - Made RDS publicly_accessible = true (for DBeaver/development)
    - Added security group rule allowing 0.0.0.0/0 on port 5432
    - Added terraform outputs for RDS endpoint and connection info
    
    WARNING: This opens RDS to the internet!
    TODO: Change cidr_blocks from 0.0.0.0/0 to YOUR_IP/32 for production
    
    This allows:
    1. EC2 containers to connect to database
    2. DBeaver/pgAdmin to connect directly from your machine

--- infra/rds.tf
+++ infra/rds.tf
@@ -12,6 +12,23 @@ resource "aws_security_group" "rds" {
     security_groups = [aws_security_group.ecs_tasks.id]
   }
 
+  # Allow inbound from EC2 instance
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ec2.id]
+  }
+
+  # Allow inbound from your IP (for DBeaver access)
+  # TODO: Replace 0.0.0.0/0 with your specific IP address for better security
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # WARNING: Open to world! Change to YOUR_IP/32
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -45,11 +62,27 @@ resource "aws_db_instance" "postgres" {
   password = "budget_password"
 
   skip_final_snapshot    = true
-  publicly_accessible    = false
+  publicly_accessible    = true  # WARNING: For development only! Restrict security group to your IP
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.rds.id]
 
   tags = {
     Name = "budget-tracker-db"
+  }
+}
+
+output "rds_endpoint" {
+  description = "RDS endpoint for database connection"
+  value       = aws_db_instance.postgres.address
+}
+
+output "database_connection_info" {
+  description = "Database connection information"
+  value = {
+    host     = aws_db_instance.postgres.address
+    port     = 5432
+    database = "budget_tracker"
+    username = "budget_user"
+    # Password not shown for security
   }
 }
